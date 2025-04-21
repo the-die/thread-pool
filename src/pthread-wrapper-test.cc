2c28ce0c6241407b16b4917d1676097624246233
the-die <xiulieshan@gmail.com>
2025-04-22 07:55:27 +0800

    update

--- src/pthread-wrapper-test.cc
+++ src/pthread-wrapper-test.cc
@@ -2,49 +2,42 @@
 
 #include <dlfcn.h>
 #include <pthread.h>
+#include <sys/syscall.h>
+#include <unistd.h>
 
-#include <cstdio>
+#include <print>
 
 void print_current_thread_stack_size()
 {
     pthread_attr_t attr;
     size_t stack_size = 0;
 
-    // Get current thread's attributes
     if (pthread_getattr_np(pthread_self(), &attr) != 0)
     {
         perror("pthread_getattr_np");
         return;
     }
 
-    // Extract stack size
     if (pthread_attr_getstacksize(&attr, &stack_size) != 0)
     {
         perror("pthread_attr_getstacksize");
         return;
     }
 
-    printf("Current thread stack size: %zu bytes\n", stack_size);
+    auto tid = (pid_t)syscall(SYS_gettid);
+    std::println("{} Current thread stack size: {} bytes", tid, stack_size);
 
-    // Clean up
     pthread_attr_destroy(&attr);
 }
 
 int main()
 {
-    auto handle = dlopen("./libpthread-wrapper.so", RTLD_NOW | RTLD_GLOBAL);
-    if (!handle)
-    {
-        printf("Failed to open new library: %s\n", dlerror());
-        return 1;
-    }
-    BS::light_thread_pool pool(16);
+    BS::light_thread_pool pool(8);
     for (int i = 0; i < 10; ++i)
     {
-        pool.submit_task(
-            [i]()
+        [[maybe_unused]] auto future = pool.submit_task(
+            []()
             {
-                std::printf("[debug] %d\n", i);
                 print_current_thread_stack_size();
             });
     }
--- src/pthread-wrapper.c
+++ src/pthread-wrapper.c
@@ -2,16 +2,18 @@
 #include <pthread.h>
 #include <stdio.h>
 
-static const size_t KiB = 1024;
-static const size_t MiB = 1024 * KiB;
-static const size_t GiB = 1024 * MiB;
-
 int pthread_create(pthread_t* restrict thread, const pthread_attr_t* restrict attr, typeof(void*(void*))* start_routine, void* restrict arg)
 {
+    static const size_t KiB = 1024;
+    static const size_t MiB = 1024 * KiB;
+    static const size_t GiB = 1024 * MiB;
     static bool initialized = false;
     static pthread_attr_t new_attr;
+
     printf("[debug] [%s%d]\n", __FILE_NAME__, __LINE__);
-    if (initialized == false)
+
+    (void)attr;
+    if (!initialized)
     {
         initialized = true;
         pthread_attr_init(&new_attr);
